f84f2798904a999c57c57c66cd2b3ae2d4b1415e
akashjaiswal36 <akash.jaiswal36@gmail.com>
2025-07-29 02:56:45 +0530

    creted aws pipeline

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull ajs3ra8/aws-sample-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 ajs3ra8/aws-sample-python-flask-app
