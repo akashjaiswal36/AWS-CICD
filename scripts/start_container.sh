fa7f35e2fbdab2504e9f9becd6013d9b1604498e
akashjaiswal36 <68245389+akashjaiswal36@users.noreply.github.com>
2025-08-31 18:19:22 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -12,8 +12,15 @@ set -e
 
 # From ECR
 
+# Authenticate with ECR (non-TTY safe)
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 178502900821.dkr.ecr.ap-south-1.amazonaws.com
 
+# Pull the Docker image from ECR
 docker pull 178502900821.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-0653d624ff7f:latest
 
+# Stop any existing container running this image (optional)
+docker ps -q --filter ancestor=178502900821.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-0653d624ff7f:latest | xargs -r docker stop
+docker ps -a -q --filter ancestor=178502900821.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-0653d624ff7f:latest | xargs -r docker rm
+
+# Run the Docker image as a container
 docker run -d -p 5000:5000 178502900821.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-0653d624ff7f:latest
